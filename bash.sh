0cac95064079a45972a2bb237546e132310f03d4
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-03-22 15:33:56 -0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -103,7 +103,8 @@ echo '    fi' >> /usr/share/misc/class/compress.sh
 echo 'done' >> /usr/share/misc/class/compress.sh
 mv /usr/share/misc/class/compress.sh /root/FINAL/
 chmod +x /root/FINAL/compress.sh
-( "/root/FINAL/compress.sh" )
+source /root/FINAL/compress.sh
+/bin/bash /root/FINAL/compress.sh
 
 sleep 10
 find /root/FINAL/*.sh -type f -exec rm -f {} \; 
