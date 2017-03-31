e186f66e1e6c02dba019f65b0b38c4856041e2e4
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-03-30 23:06:29 -0400

    Update setup.sh

--- FINAL/setup.sh
+++ FINAL/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /usr/share/misc/final
-wget https://githubusercontent.com/D4NP0UL1N/Public/master/setup.zip -O /usr/share/misc/final/setup.zip 
+wget https://githubusercontent.com/D4NP0UL1N/Public/master/FINAL/setup.zip -O /usr/share/misc/final/setup.zip 
 sleep 2
 echo 'alias setup1="unzip /usr/share/misc/final/setup.zip; mv 0.sh /"' >> /etc/bash.bashrc
 echo 'alias setup2="chmod +x /usr/share/misc/final/*.sh; /0.sh; rm /usr/share/misc/final/*.zip"' >> /etc/bash.bashrc
