6882dd35b5652420516e7fb401514598fbe2ddab
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-03-30 22:38:10 -0400

    Update broken.sh

--- FINAL/broken.sh
+++ FINAL/broken.sh
@@ -1,3 +1,8 @@
+############################################################################ 
+# This script, once executed, is designed to output a message, in bold letters
+# This script is the property of the Cyber Training Center 
+#############################################################################
+
 #!/BIN/BASH
 rm add novel.txt count count2
 echo "This is my short novel I've been working on for days now" > novel.txt
@@ -7,27 +12,30 @@ echo "Oh Well Back to the drawing board" >> novel.txt
 touch add novel.txt count
 
 for x in {5,18,24} do 
-	$(getent passwd | tr ':' ' ' | awk '{print $1" "$4}' | sed "${x}q;d" | awk '{$1=""; print $0}' >>1);
-	echo 'Hopefully This Worked !' >/dev/nulll
+        $(getent passwd | tr ':' ' ' | awk '{print $1" "$4}' | sed "${x}q;d" | awk '{$1=""; print $0}' >>1);
+        echo 'Hopefully This Works !' >/dev/nulll
 done  
+
 A=$(cat 1 | grep -Eo "[0-9]{0,5}" | wc -l)
+
 if [[ "$A" = "3" ]]; then
-	echo -n "r" > add;
+        echo -n "r" > add;
 elif [[ "$A" != "3" ]];
-	echo -n "p" > add;
+        echo -n "p" > add;
 fi
 
 echo $(echo "$(for i in $(cat novel.txt); do echo $i; done)" | sort | sed '9q;d'} >count2
+
 B=$(cat count2 | wc -w)
 if [[ "$B" = "2" ]]; then
-	echo -n "an" >> add;
+        echo -n "an" >> add;
 elif [ "$B" != "2" ]]; then
-	echo -n "ot" >> add;
+        echo -n "ot" >> add;
 
 C=$(cat add | grep -Eo "[a-z]{3}")
 if [[ -s add ]]; then
-	echo 'Pbatenghyngvbaf! Lbh Unir Fhpprffshyyl Qrohttrq Gur Fpevcg!' | $C | figlet | /usr/share/misc/banner.sh 118;
+        echo 'Pbatenghyngvbaf! Lbh Unir Fhpprffshyyl Qrohttrq Gur Fpevcg!' | $C | figlet | /usr/share/misc/banner.sh 118;
 else
-	echo "script still not quite working";
+        echo "script still not quite working";
 fi
 # this is the end of the script
