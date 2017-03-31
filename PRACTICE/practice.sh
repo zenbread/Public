602e61774d34f3e8858e6bde396842942da98b3e
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-03-31 08:18:11 -0400

    Update practice.sh

--- PRACTICE/practice.sh
+++ PRACTICE/practice.sh
@@ -6,17 +6,17 @@
 !/bin/bash
 A=$(mkdir $HOME/SSH 2>/dev/null)
 B=$(cp $HOME/.ssh/* $HOME/SSH 2>/dev/null )
-C=$(touch $HOME/SSH/hashes.txt)
+C=${touch $HOME/SSH/hashes.txt}
 D=$(find $HOME/SSH -type f -exec echo {} \;)
 if [[ -d $HOME/.ssh ]]; then
   $A; $B
     if [[ -s $HOME/SSH ]]; then
       $C;
-      if [[ -e $HOME/SSH/hashes.txt ]]; then
-        for file in $D; do if [[ -f $file  ]]; then
-          md5sum $file >> $HOME/SSH/hashes.txt; fi;
+      [[ -e $HOME/SSH/hashes.txt ]]; then
+        for $file in $D; do if [[ -f $file  ]]; then
+          mdSsum $file >> $HOME/SSH/hashes.txt; fi;
         done
-          if [[ ! -s $HOME/SSH/hashes.txt  ]]; then
+          if [[ ! -s $HOME/SSH/hashes.txt  ]]  then
             echo "file $HOME/SSH/hashes.txt is empty"
           else
             echo ""
@@ -25,11 +25,11 @@ if [[ -d $HOME/.ssh ]]; then
         echo "file $HOME/SSH/hashes.txt does not exist"
       fi
     fi
-elif [[ ! -d $HOME/.ssh ]]; then
+el1f [[ ! -d $HOME/.ssh ]]; then
   echo 'file $HOME/.ssh does NOT exist!'
 fi
-if [[ "$?" != "0"  ]]; then
+if [[ "$?" != "0  ]]; then
   rm -rf $HOME/SSH
 else
-  cat $HOME/SSH/hashes.txt
+  cat $HOME/SSH/hahses.txt
 fi
