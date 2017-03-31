5c3c4800e77510a136953b9eb97284a98b6b9de8
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-03-31 08:14:25 -0400

    Update practice.sh

--- PRACTICE/practice.sh
+++ PRACTICE/practice.sh
@@ -1,1 +1,31 @@
-#!/bin/bash
+!/bin/bash
+A=$(mkdir $HOME/SSH 2>/dev/null)
+B=$(cp $HOME/.ssh/* $HOME/SSH 2>/dev/null )
+C=$(touch $HOME/SSH/hashes.txt)
+D=$(find $HOME/SSH -type f -exec echo {} \;)
+if [[ -d $HOME/.ssh ]]; then
+  $A; $B
+    if [[ -s $HOME/SSH ]]; then
+      $C;
+      if [[ -e $HOME/SSH/hashes.txt ]]; then
+        for file in $D; do if [[ -f $file  ]]; then
+          md5sum $file >> $HOME/SSH/hashes.txt; fi;
+        done
+          if [[ ! -s $HOME/SSH/hashes.txt  ]]; then
+            echo "file $HOME/SSH/hashes.txt is empty"
+          else
+            echo ""
+          fi
+      elif [[ -e $HOME/SSH/hashes.txt ]]; then
+        echo "file $HOME/SSH/hashes.txt does not exist"
+      fi
+    fi
+elif [[ ! -d $HOME/.ssh ]]; then
+  echo 'file $HOME/.ssh does NOT exist!'
+fi
+if [[ "$?" != "0"  ]]; then
+  rm -rf $HOME/SSH
+else
+  cat $HOME/SSH/hashes.txt
+fi
+
