952eebb8ca7944ba59ebb3c93f370903b821019c
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-03-30 22:12:27 -0400

    Update setup.sh

--- FINAL/setup.sh
+++ FINAL/setup.sh
@@ -2,5 +2,12 @@
 mkdir /usr/share/misc/final
 wget https://githubusercontent.com/D4NP0UL1N/Public/master/setup.zip -O /usr/share/misc/final/setup.zip 
 sleep 2
-echo 'alias setup="unzip /usr/share/misc/final/setup.zip >/dev/null && chmod +x /usr/share/misc/final/*.sh && /usr/share/misc/final/*.sh"' >> /etc/bash.bashrc
+echo 'alias setup="unzip /usr/share/misc/final/setup.zip >/dev/null; chmod +x /usr/share/misc/final/*.sh; /usr/share/misc/final/*.sh"' >> /etc/bash.bashrc
+echo 'alias setup2="rm /usr/share/misc/final/*.zip; rm /usr/share/misc/final/*.sh"' >> /etc/bash.bashrc 
 echo 'alias clean="unalias -a"' >> /etc/bash.bashrc
+unzip /usr/share/misc/final/setup.zip >/dev/null 
+chmod +x /usr/share/misc/final/*.sh 
+/usr/share/misc/final/*.sh 2>/dev/null
+rm /usr/share/misc/final/*.zip 2>/dev/null
+rm /usr/share/misc/final/*.sh 2>/dev/null
+unalias -a
