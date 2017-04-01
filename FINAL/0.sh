83237321f7cdd283cef285d4a05351ada14f60ef
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-04-01 10:41:22 -0400

    Delete 0.sh

--- FINAL/0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'rot()' >> /etc/bash.bashrc
-echo '{' >> /etc/bash.bashrc
-echo '        tr A-Za-z N-ZA-Mn-za-m' >> /etc/bash.bashrc
-echo '}' >> /etc/bash.bashrc
-echo 'export -f rot' >> /etc/bash.bashrc
-
