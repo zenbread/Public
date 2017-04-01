f171169c129c20588467d4859af277f79434c318
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-04-01 10:42:42 -0400

    Delete 2.sh

--- FINAL/2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/D4NP0UL1N/Public/master/FINAL/0.sh -O /usr/share/misc/final/0.sh 
-echo 'alias fix="mv /usr/share/misc/final/0.sh /; /0.sh"' >> /etc/bash.bashrc
-echo 'alias clean="unalias -a; find / -name 0.sh -exec rm -f {} 2>/dev/null \;"' >> /etc/bash.bashrc
