5a506d665714d65234f735f8fac51ae1f2cd8ce0
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-04-13 16:50:50 -0400

    Create setup1.ps1

---
+++ AD/setup1.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = 'SilentlyContinue'
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath 'C:\Windows\NTDS' `
+-DomainMode 'Win2012R2' `
+-DomainName 'army.warriors' `
+-DomainNetbiosName 'ARMY' `
+-ForestMode 'Win2012R2' `
+-SafeModeAdministratorPassword (ConvertTo-SecureString -String 'ReallyStrongPassword!!' -AsPlainText -Force) `
+-InstallDns:$true `
+-LogPath 'C:\Windows\NTDS' `
+-NoRebootOnCompletion:$true `
+-SysvolPath 'C:\Windows\SYSVOL' `
+-Force:$true
+Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" "setup2" 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noprofile -sta -File "C:\setup2.ps1"'
+Restart-Computer -Force
