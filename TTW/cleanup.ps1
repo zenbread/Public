9c5256386abb4f222c0887f319584ead16f4d275
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-05-31 19:06:26 -0400

    Update cleanup.ps1

--- TTW/cleanup.ps1
+++ TTW/cleanup.ps1
@@ -1,3 +1,4 @@
 $ErrorActionPreference = 'SilentlyContinue'
 Remove-Item C:\windows\system32\setup1.ps1 -Force
 Remove-Item C:\windows\system32\setup2.ps1 -Force
+Remove-Item C:\windows\system32\psexec.exe -Force
