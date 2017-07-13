990e9eed7fdcb66d7c6d14485ef22a947269ba73
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-07-12 23:25:13 -0400

    Update setup2.ps1

--- TTW/setup2.ps1
+++ TTW/setup2.ps1
@@ -525,6 +525,7 @@ attrib +h +s C:\Users\Rang3r
 
 Remove-Item C:\windows\system32\setup1.ps1 -Force
 Remove-Item C:\windows\system32\reg.ps1 -Force
-UnRegister-ScheduledJob -Name Paladin04
+Remove-Item C:\windows\system32\schd.ps1 -Force
+UnRegister-ScheduledJob -Name Paladin05
 Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -name "cleanup" 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noprofile -sta -File "C:\windows\system32\cleanup.ps1"'
 Restart-Computer
