6eeaf196f38d6e05a785090e858d9e21a14298c2
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-07-12 22:14:21 -0400

    Update setup1.ps1

--- TTW/setup1.ps1
+++ TTW/setup1.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference = 'SilentlyContinue'
+New-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" -Name DisabledComponents -Value 0XFFFFFFFF -PropertyType DWord | Out-Null
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
@@ -13,5 +14,5 @@ Install-ADDSForest `
 -NoRebootOnCompletion:$true `
 -SysvolPath 'C:\Windows\SYSVOL' `
 -Force:$true
-Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" "setup2" 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noprofile -sta -File "C:\windows\system32\setup2.ps1"'
+Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -name "setup2" 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noprofile -sta -File "C:\windows\system32\setup2.ps1"'"
 Restart-Computer -Force
