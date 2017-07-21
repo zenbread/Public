9c178662664f421dc2e3c08048ce235decb649e0
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-07-21 12:51:13 -0400

    Update cleanup.ps1

--- TTW/cleanup.ps1
+++ TTW/cleanup.ps1
@@ -1,4 +1,2 @@
 $ErrorActionPreference = 'SilentlyContinue'
-icacls C:\windows\system32\setup1.ps1 /deny BUILTIN\Users:RX
-icacls C:\windows\system32\setup2.ps1 /deny BUILTIN\Users:RX
-Remove-Item C:\windows\system32\psexec.exe -Force
+icacls C:\windows\system32\psexec.exe /deny BUILTIN\Users:RX
