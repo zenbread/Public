324c47fd254b5eb7edda39ca485a839df6db0fd0
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-07-24 19:01:57 -0400

    Update start.ps1

--- TTW/start.ps1
+++ TTW/start.ps1
@@ -9,4 +9,7 @@ start-sleep -s 1
 Remove-Item C:\windows\system32\setup2.ps1 -force
 start-sleep -s 3
 
+Write-Output "The password for the next level is the Powershell build version. Please be sure to include all periods" -n > C:\Users\Apprentice01\Desktop\challenge.txt
+icacls "C:\Users\Apprentice01\Desktop\challenge.txt" /grant Apprentice01:F /T /C
+
 restart-computer
