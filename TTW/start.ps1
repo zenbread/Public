c9093ba3880b65661bb1ecb847cfcc8f027ac48c
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-07-24 18:30:44 -0400

    Update start.ps1

--- TTW/start.ps1
+++ TTW/start.ps1
@@ -1,14 +1,5 @@
 $ErrorActionPreference = 'SilentlyContinue'
 
-#icacls "C:\windows\system32\psexec.exe" /deny BUILTIN\Users:RX
-#start-sleep -s 1
-
-#icacls "C:\windows\system32\setup1.ps1" /deny BUILTIN\Users:RX
-#start-sleep -s 1
-
-#icacls "C:\windows\system32\setup2.ps1" /deny BUILTIN\Users:RX
-#start-sleep -s 1
-
 Remove-Item C:\windows\system32\psexec.exe -force
 start-sleep -s 1
 
