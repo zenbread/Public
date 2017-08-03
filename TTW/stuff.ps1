33f61c42e78681ac7425b916596d3564c08a9b48
zenbread <nmcamp87@gmail.com>
2017-08-03 18:09:58 -0400

    Update stuff.ps1

--- TTW/stuff.ps1
+++ TTW/stuff.ps1
@@ -179,7 +179,7 @@ Restart-Computer
 
 New-Item $PROFILE.AllUsersAllHosts -ItemType File -Force
 echo "$ProfileRoot = (Split-Path -Parent $MyInvocation.MyCommand.Path)" > "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
-echo "$env:path += '$ProfileRoot'" >> "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
+echo "$env:path += "$ProfileRoot"" >> "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
 icacls C:\WINDOWS\system32\WindowsPowerShell\v1.0\start.ps1 /grant Everyone:F /T /C
 icacls C:\Windows\System32\setup2.ps1 /grant Everyone:F /T /C
 icacls C:\Windows\System32\PsExec.exe /grant Everyone:F /T /C
