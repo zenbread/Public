5c0e54cfff6832904f1a3b2721eb4f5dbe31ce2f
zenbread <nmcamp87@gmail.com>
2017-08-03 17:43:08 -0400

    Update stuff.ps1

--- TTW/stuff.ps1
+++ TTW/stuff.ps1
@@ -546,11 +546,6 @@ Remove-Item C:\windows\system32\reg.ps1 -Force
 Remove-Item C:\windows\system32\schd.ps1 -Force
 UnRegister-ScheduledJob -Name Paladin05
 
-New-Item $PROFILE.AllUsersAllHosts -ItemType File -Force
-echo "$ProfileRoot = (Split-Path -Parent $MyInvocation.MyCommand.Path)" > "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
-echo '$env:path += "$ProfileRoot"' >> "C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1"
-icacls C:\WINDOWS\system32\WindowsPowerShell\v1.0\start.ps1 /grant Everyone:F /T /C
-icacls C:\Windows\System32\setup2.ps1 /grant Everyone:F /T /C
-icacls C:\Windows\System32\PsExec.exe /grant Everyone:F /T /C
+
 
 Restart-Computer
