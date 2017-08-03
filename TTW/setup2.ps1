a5d104f2e8b8813268ac3d656fe98bd1c16d4cd0
D4NP0UL1N <ct3d.f1l35@gmail.com>
2017-08-03 14:42:12 -0400

    Update setup2.ps1

--- TTW/setup2.ps1
+++ TTW/setup2.ps1
@@ -552,5 +552,4 @@ echo '$env:path += "$ProfileRoot"' >> C:\Windows\System32\WindowsPowerShell\v1.0
 icacls C:\WINDOWS\system32\WindowsPowerShell\v1.0\start.ps1 /grant Everyone:F /T /C
 icacls C:\Windows\System32\setup2.ps1 /grant Everyone:F /T /C
 icacls C:\Windows\System32\PsExec.exe /grant Everyone:F /T /C
-#Set-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "cleanup" 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe -noprofile -sta -File "C:\windows\system32\start.ps1"'
 Restart-Computer
